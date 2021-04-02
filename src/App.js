15910844d7f4553609d3c3b02107a73b33be2c98
izaak svec <slavi.svec@gmail.com>
2021-04-02 14:18:55 +1100

    Added eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import './App.css'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import About from './pages/About/About'
-
 
 const App = () => {
   return (
-      <>
+    <>
       <Router>
         <Switch>
-          <Route path='/' exact component={About} />
+          <Route path="/" exact component={About} />
         </Switch>
       </Router>
     </>
   )
 }
 
-export default App;
+export default App
