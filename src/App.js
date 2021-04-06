1e38a379b8609fb3dcd9e869e8650e7d6beed516
izaak svec <slavi.svec@gmail.com>
2021-04-06 10:28:51 +1000

    Update footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar/Navbar'
 import About from './pages/About/About'
 import Work from './pages/Work/Work'
@@ -16,6 +17,7 @@ const App = () => {
           <Route path="/work" component={Work} />
           <Route path="/contact" component={Contact} />
         </Switch>
+        <Footer />
       </Router>
     </>
   )
